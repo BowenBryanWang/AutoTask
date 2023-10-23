53cc7efb8ca0d40db014b2a0691b414b9edb9aa8
张翔宇 <2022012081@secoder.net>
2023-10-23 23:39:55 +0800

    feat:LLM-hist-5-CoT completed. Task could end

--- main.py
+++ main.py
@@ -17,18 +17,15 @@ app = Flask(__name__)
 
 TASK = ""
 MODE = ""
+MODEL = ""
 STATUS = "stop"
 INDEX = 0
-ACTION_TRACE = {
-    "ACTION": [],
-    "ACTION_DESC": [],
-    "TRACE": [],
-    "TRACE_DESC": [],
-}
+NEW_TASK = True
+ACTION_TRACE = []
 
 @app.route('/demo', methods=['POST'])
 def demo() -> Union[str, Response]:
-    global TASK, STATUS, ACTION_TRACE
+    global TASK, STATUS, ACTION_TRACE, NEW_TASK
     screen = Screen(INDEX)
     screen.update(request=request.form)
 
@@ -39,18 +36,35 @@ def demo() -> Union[str, Response]:
     print("Screen UI semantic list 形式:", screen.semantic_info_list)
 
     if STATUS == "start":
+        NEW_TASK = True
+        ACTION_TRACE = []
         STATUS = "running"
-        model = Model(screen=screen, description=TASK)
-        prompt = paper_provided_prompt(model.task, ACTION_TRACE, model.screen.semantic_info_list)
-        result = GPT(prompt)
-        print(result)
-        return result
-    return Response("0")
     
+    if STATUS == "running":
+        model = Model(screen=screen, description=TASK)
 
-    # TODO：任务
-    # 照抄论文中的prompt，然后把刚刚这些变量和用户任务TASK输入到GPT调用里面，得到返回结果后处理好，然后return掉即可。
-    # GPT如何调用？已经写好接口，参考./src/Utility.py中的GPT函数，和./src/Utility.py中的GPT调用示例 Task_grounding()函数
+        query_history = read_history()
+
+        if MODEL == "LLM-0":
+            prompt = paper_provided_prompt_1(model.task, model.screen.semantic_info_list)
+        elif MODEL == "LLM-hist-5-CoT":
+            prompt = paper_provided_prompt_2(model.task, query_history, model.screen.semantic_info_list)
+
+        result = GPT(prompt)
+        
+        print(result)
+
+        save_history(query_history, model.screen.semantic_info_list, model.task, result)
+
+        if result["action_type"] == "report_answer":
+            STATUS = "end"
+            ACTION_TRACE = []
+        else:
+            ACTION_TRACE.append(result)
+
+        return result
+    return None
+
     # i.e.
     # result = GPT(task_grounding_prompt(self.model.task,self.model.similar_tasks, self.model.similar_traces, ACTION_TRACE, self.model.screen.semantic_info_list))
     # self.model.predicted_step = result["result"]
@@ -132,7 +146,7 @@ def demo() -> Union[str, Response]:
     #         print("------------------------back--------------------------")
     #         return {"node_id": 1, "trail": "[0,0]", "action_type": "back"}
 
-def paper_provided_prompt(task, previous_action, current_ui):
+def paper_provided_prompt_1(task, current_ui):
     return [
         {
             "role": "system",
@@ -150,19 +164,90 @@ Available Actions:
 {"action_type": "scroll", "direction": "down"}
 {"action_type": "scroll", "direction": "left"}
 {"action_type": "scroll", "direction": "right"}
+{"action_type": "report_answer", "text": <answer_to_the_instruction>}
 """
         },
         {
             "role": "user",
             "content": 
 """
+Screen: {}
+Instruction: {}
+Think step by step, and output JSON format with property name enclosed in double quotes. 
+When found the answer or finished the task, use "report_answer" to report the answer or to end the task.
+Answer:
+""".format(current_ui, task)
+        }
+    ]
+
+def paper_provided_prompt_2(task, query_history, current_ui):
+    prompt = []
+    prompt.append({
+            "role": "system",
+            "content": 
+"""
+Given a mobile screen and a question, provide the action based on the screen
+information.
+
+Available Actions:
+{"action_type": "click", "idx": <element_idx>}
+{"action_type": "type", "text": <text>}
+{"action_type": "navigate_home"}
+{"action_type": "navigate_back"}
+{"action_type": "scroll", "direction": "up"}
+{"action_type": "scroll", "direction": "down"}
+{"action_type": "scroll", "direction": "left"}
+{"action_type": "scroll", "direction": "right"}
+{"action_type": "report_answer", "text": <answer_to_the_instruction>}
+"""
+        })
+    
+    for query in query_history:
+        prompt.append({
+            "role": "user",
+            "content": 
+"""
 Previous Actions: {}
 Screen: {}
 Instruction: {}
-Think step by step, and output JSON format.
-""".format(previous_action, current_ui, task)
-        }
-    ]
+Answer: {}
+""".format(query["Previous Actions"], query["Screen"], query["Instruction"], query["Answer"])
+        })
+
+    prompt.append({
+            "role": "user",
+            "content": 
+"""
+Screen: {}
+Instruction: {}
+Think step by step, and output JSON format with property name enclosed in double quotes. When found the answer or finished the task, use "report_answer" to report the answer or to end the task.
+Answer:
+""".format(current_ui, task)
+        })
+    return prompt
+
+def read_history():
+    with open("./src/KB/history.json", "r") as f:
+        history = json.load(f)
+    return history
+
+def save_history(query_history, screen, instruction, answer):
+    global ACTION_TRACE, NEW_TASK
+    history = {
+        "Previous Actions": ACTION_TRACE,
+        "Screen": screen,
+        "Instruction": instruction,
+        "Answer": answer
+    }
+    if NEW_TASK:
+        while len(query_history) >= 5:
+            query_history.pop()
+        query_history.insert(0, history)
+        NEW_TASK = False
+    else:
+        query_history[0] = history
+    with open("./src/KB/history.json", "w") as f:
+        json.dump(query_history, f, indent=4)
 
 def save_to_file(task_name):
     task_name = task_name.replace(" ", "_")
@@ -180,8 +265,8 @@ def on_key_release(key):
         STATUS = "start"
         print("Task execution started!")
     elif key == keyboard.Key.delete:
-        STATUS = "backtracking"
-        print("Backtracking started!")
+        STATUS = "end"
+        print("Task ended!")
 
 
 def keyboard_listener():
@@ -196,10 +281,13 @@ if __name__ == "__main__":
                         default="View data usage of 'T-mobile' on this phone")
     parser.add_argument("--mode", type=str, choices=["normal", "preserve"],
                         default="normal", help="Specify the mode: 'normal' or 'preserve'")
+    parser.add_argument("--model", type=str, choices=["LLM-0", "LLM-hist-5-CoT"],
+                        default="LLM-0", help="Specify the model: 'LLM-0' or 'LLM-hist-5-CoT'")
     args = parser.parse_args()
 
     TASK = args.task
     MODE = args.mode
+    MODEL = args.model
 
     keyboard_thread = threading.Thread(target=keyboard_listener)
     keyboard_thread.daemon = True
