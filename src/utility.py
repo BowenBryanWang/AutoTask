8b9749f6c8420e3659bc47a43fdc945ea76416d5
Saltyp0rridge <wbw20@mails.tsinghua.edu.cn>
2023-10-23 15:47:58 +0800

    feat: Feedback update

--- src/utility.py
+++ src/utility.py
@@ -459,11 +459,11 @@ def decide_prompt(task, ACTION_TRACE, semantic_info, Knowledge):
         "content": """You are a professor with in-depth knowledge of User Interface (UI) tasks. You are assigned a specific UI task, a history operation sequence, and the current UI (which is the result of the operation sequence).
 Your task is to evaluate if the action trace aligns with the assigned task and if the current UI is related to the UI task. You should categorize the operation sequence as:
 1,completed: After the Latest action the subsequent newest UI screen, the user's task is completed;
-2,wrong: After the Latest action the subsequent newest UI screen, the history ACTION sequence is not correct and the current UI is not related to the UI task;
-3,go on: After the Latest action the  subsequent newest UI screen, the history ACTION sequence is on the correct track, which means the agent can continue to perform further operations to complete the task. Further Actions should be taken on the current (may also be the subsequent pages) UI page.
+2,wrong: After the Latest action the subsequent newest UI screen, the history operation sequence is not correct and the current UI is not related to the UI task;
+3,go on: After the Latest action the  subsequent newest UI screen, the history operation sequence is on the correct track, which means the agent can continue to perform further operations to complete the task. Further Actions should be taken on the current (may also be the subsequent pages) UI page.
 Use the following steps to respond to user inputs. Fully restate each step number before proceeding. i.e. "Step 1".
-Step 1:Reason step-by-step about the relationship of the history ACTION sequence and UI task.
-Step 2:Reason step-by-step about whether the latest ACTION and subsequent UI screen are relevant to the UI task. Is there any element on the screen is related with the task and you think that it should be operated next?
+Step 1:Reason step-by-step about the relationship of the history ACTION sequence and UI task. Whether the sequence helps fulfill the user's task semantically?
+Step 2:Reason step-by-step about whether the latest ACTION and subsequent UI SCREEN are relevant to the UI task. Is there any element on screen that is related with the task and you think it should be operated next?
 Step 3:Output a JSON object structured like: 
 {
     "status": "completed" or "wrong" or "go on", 
