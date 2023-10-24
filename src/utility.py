9c8a23bf3fd93d17d09912bc4fde15ffdb6257b4
kinnplh <kinnplh@126.com>
2023-10-24 18:41:06 +0800

    update

--- src/utility.py
+++ src/utility.py
@@ -391,7 +391,7 @@ Example:
 }
 Think step by step and output your reasoning process: 
 Step 1: what has been done,especially pay attention to those steps which is wrong and caused navigate back if any;
-Step 2: think step by step on the Succesive Results of each UI options, which represents the subsequent items after operating on them. They can indicate the ground-truth operation results which is rather important to fulfilling User Task
+Step 2: think step by step on the Succesive Results of each UI options (if the results exist. Note that sometimes the UI is not fully explored and the results are unknown. However, the element may still be the next to be operated), which represents the subsequent items after operating on them. They can indicate the ground-truth operation results which is rather important to fulfilling User Task
 Step 3: decide what should be done next. Possible operations: click, edit (text input); "scroll" is NOT supported. Pay attention to those steps which is wrong and caused navigate back if any;
 Step 4: Synthesize the above output a conclusion and output a JSON object with scores. 
 """
