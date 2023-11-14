07d1954543c5f4f18b3a11853a18174d27c8ea27
BowenBryanWang <wbw20@mails.tsinghua.edu.cn>
2023-11-14 22:25:50 +0800

    feat: update phrase2

--- src/knowledge.py
+++ src/knowledge.py
@@ -5,7 +5,7 @@ import pickle
 import random
 
 
-from utility import GPT, Knowledge_prompt, get_top_combined_similarities, get_top_similarities, sort_by_similarity
+from src.utility import GPT, Knowledge_prompt, get_top_combined_similarities, get_top_similarities, sort_by_similarity
 
 OPENAI_KEY = os.getenv('OPENAI_API_KEY')
 COUNT = 0
